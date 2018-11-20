cca070f01711a0a32acd224423203ce678c8c104
Tomi <tomas.canosa@hotmail.com>
2018-11-20 01:16:06 -0300

    Standardization, full segues
    MVC implementation, segues with ID and unwinders

---
+++ Swoosh/Swoosh/Controller/DesiredLeagueViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DesiredLeagueViewController.swift
+//  Swoosh
+//
+//  Created by Tomi on 20/11/2018.
+//  Copyright © 2018 Tomi. All rights reserved.
+//
+
+import UIKit
+
+class DesiredLeagueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindFromDesiredSeniorityViewController (unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+}
---
+++ Swoosh/Swoosh/Controller/DesiredSeniorityViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DesiredSeniorityViewController.swift
+//  Swoosh
+//
+//  Created by Tomi on 20/11/2018.
+//  Copyright © 2018 Tomi. All rights reserved.
+//
+
+import UIKit
+
+class DesiredSeniorityViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
